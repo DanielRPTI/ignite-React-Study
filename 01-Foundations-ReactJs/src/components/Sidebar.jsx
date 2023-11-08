6705c2bb616f70e24dc407f81e5c879729ba5026
DanielRPTI <drjunior988@hotmail.com>
2023-11-08 08:38:09 -0300

    feat:finalizando estilizações na Sidebar

--- 01-Foundations-ReactJs/src/components/Sidebar.jsx
+++ 01-Foundations-ReactJs/src/components/Sidebar.jsx
@@ -1,15 +1,23 @@
 import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
 export function Sidebar () {
   return (
     <aside className={styles.sidebar}>
-      <img className= {styles.cover} 
-      src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=60=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D " alt="" />
+      <img 
+        className= {styles.cover} 
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=60=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "
+      />
       <div  className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/DanielRPTI.png" alt="" />
+
         <strong>Daniel Rosa</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar o seu perfil </a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil 
+        </a>
       </footer>
     </aside>
   )
