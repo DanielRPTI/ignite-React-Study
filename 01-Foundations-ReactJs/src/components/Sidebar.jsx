ab367d950a0595a5ee00fdfcd60eff8bd41f8a3d
DanielRPTI <drjunior988@hotmail.com>
2023-11-08 12:47:28 -0300

    feat: Criando o Componenete Avatar

--- 01-Foundations-ReactJs/src/components/Sidebar.jsx
+++ 01-Foundations-ReactJs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import {PencilLine} from 'phosphor-react'
 export function Sidebar () {
@@ -8,8 +9,7 @@ export function Sidebar () {
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=60=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "
       />
       <div  className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/DanielRPTI.png" alt="" />
-
+        <Avatar hasBorder src="https://github.com/DanielRPTI.png" />
         <strong>Daniel Rosa</strong>
         <span>Web Developer</span>
       </div>
