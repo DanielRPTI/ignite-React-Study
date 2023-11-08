7ca5d20fa385c4aadeb8a2479fcd099769eabaf3
Daniel Rosa Pinheiro Junior <daniel.junior@unimed-franca>
2023-11-07 22:44:27 -0300

    feat: Criando e estilizando a Sidebar do projeto

--- 01-Foundations-ReactJs/src/App.jsx
+++ 01-Foundations-ReactJs/src/App.jsx
@@ -1,10 +1,25 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 import './global.css'
+import styles from './App.module.css'
 export function App() {
   return (
     <div>
       <Header />
-
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author = "Daniel Rosa"
+            content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus beatae esse tenetur obcaecati vel nihil aliquam libero odio eum magnam laborum vero qui sed ex dignissimos asperiores, officiis accusamus? Corporis."
+          />
+          <Post
+            author = "Diego Fernandes"
+            content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus beatae esse tenetur obcaecati vel nihil aliquam libero odio eum magnam laborum vero qui sed ex dignissimos asperiores, officiis accusamus? Corporis."
+          />
+        </main>
+      </div>
     </div>
   )
 }
