d67a8dc1d8ba32651d74b3397b7c209b8aac609e
DanielRPTI <drjunior988@hotmail.com>
2023-11-09 14:22:50 -0300

    Reconfigurando o Objeto post

--- 01-fundamentals-reactjs-ts/src/App.tsx
+++ 01-fundamentals-reactjs-ts/src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
+import { PostType } from "./components/Post";
 
 import './global.css'
 import styles from './App.module.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -46,10 +47,8 @@ export function App() {
           {posts.map(post => {
            return (
               <Post 
-              key={post.id}
-                author = {post.author}
-                content = {post.content}
-                publishedAt = {post.publishedAt}
+                key={post.id}
+                post={post}
              />
             )
           })}
