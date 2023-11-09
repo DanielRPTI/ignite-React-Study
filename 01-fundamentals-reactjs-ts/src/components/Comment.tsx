ae5d51498a37019f8a9bf786c087e96bb3b44b04
DanielRPTI <drjunior988@hotmail.com>
2023-11-09 13:12:22 -0300

    Começando o projeto novamente dessa vez utilizando TypeScript

---
+++ 01-fundamentals-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css'
+import {ThumbsUp, Trash} from 'phosphor-react'
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment ({content, onDeleteComment}: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleLikeComment () {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  function handleDeleteComment () {
+    onDeleteComment(content)
+  }
+
+  return(
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/DanielRPTI.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Daniel Rosa</strong>
+              <time title='' dateTime="2023-08-11 08:57:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
